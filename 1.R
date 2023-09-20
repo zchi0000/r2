ecc729f74d35ad746ac3bc310de46fde32bcd05d
zchi0000 <144878630+zchi0000@users.noreply.github.com>
2023-09-20 15:00:41 +0800

    ...

---
+++ 1.R
@@ -0,0 +1,2 @@
+dt1 <- readr::read_csv("https://raw.githubusercontent.com/tpemartin/112-1-programming-for-data-science/main/data/cities.csv") 
+dt2 <- jsonlite::fromJSON("https://raw.githubusercontent.com/tpemartin/112-1-programming-for-data-science/main/data/f
